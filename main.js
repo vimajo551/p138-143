fbacbe87025c077dc39704464c69d3dac03e5e4d
vimajo551 <104533881+vimajo551@users.noreply.github.com>
2023-04-05 20:19:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+paddle1 = 10;
+paddle2 = 10;
+
+paddle1X = 10;
+
+paddle1Y = '';
+paddle2Y = 685;
+
+paddle1Height = 110;
+paddle2Height = 70;
+
+score1 = 0;
+score2 = 0;
+
+playerscore = 0;
+pcscore = 0;
+
 function setup(){
     canvas =  createCanvas(700,550);
     canvas.parent('canvas');
@@ -17,3 +34,49 @@ function modelLoaded(){
 function gotPoses(results){
 
 }
+
+function draw(){
+    if(gameStatus == "start")
+    {
+      background(0); 
+      image(video, 0, 0, 700, 550);
+    
+      fill("black");
+      stroke("black");
+      rect(680,0,20,700);
+    
+      fill("black");
+      stroke("black");
+      rect(0,0,20,700);
+    
+      if(scoreRightWrist > 0.2)
+      {
+        fill("red");
+        stroke("red");
+        circle(rightWristX, rightWristY, 30);
+      }
+    
+        paddleInCanvas();
+
+        fill(250,0,0);
+        stroke(0,0,250);
+        strokeWeight(0.5);
+        paddle1Y = rightWristY; 
+        rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+    
+
+        fill("#FFA500");
+        stroke("#FFA500");
+        var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+
+        midline();
+
+        drawScore();
+
+        models();
+
+        move();
+    
+        }
+    
+      }
